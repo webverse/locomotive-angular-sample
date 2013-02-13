d387af869452a4ab4a22a6dec590d083c7263e6c
Fabio Favero <webverse.systems@gmail.com>
2013-02-13 15:02:47 -0200

    Mongo dev dbname changed

--- config/environments/development.js
+++ config/environments/development.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 module.exports = function() {
   this.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  this.set('db-uri', 'mongodb://localhost:27017/screencast');
+  this.set('db-uri', 'mongodb://localhost:27017/products');
   this.set('view options', {
     pretty: true
   });
